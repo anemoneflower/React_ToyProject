9c15bb58210117fda15c72f4f4e7f8324a70a767
seoyun <sallyluckyyun@gmail.com>
2023-04-04 21:28:10 +0900

    ADD: router - TODO: upgrade react-router-dom version to latest one

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import { useEffect, useState } from "react";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home"
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [selected, setSelected] = useState("");
-  const [coinIdx, setCoinIdx] = useState(0);
-  const [myMoney, setMoney] = useState(0);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => { setCoins(json); setLoading(false) })
-  }, [])
-  const onWriteMoney = (event) => setMoney(event.target.value)
-  const onSelect = (event) => { setSelected(event.target.value); setCoinIdx(event.target.selectedIndex) }
-  return (
-    <div className="App">
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> :
-        <div>
-          <input value={myMoney > 0 ? myMoney : ""} placeholder="How much do you have?" onChange={onWriteMoney}></input>
-          <select value={selected} onChange={onSelect}>
-            {coins.map((coin) => <option key={coin.id}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price}</option>)}
-          </select>
-          <hr />
-          <h3>You can buy {myMoney / coins[coinIdx].quotes.USD.price} {coins[coinIdx].name}</h3>
-        </div>
-      }
-    </div>
-  );
+  return <Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
+
+function Movie({ coverImg, title, rating, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title}></img>
+      <h2>
+        <Link to="/movie">{title}:{rating}</Link>
+      </h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map(g => <li key={g}>{g}</li>)}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+}
+
+export default Movie;
