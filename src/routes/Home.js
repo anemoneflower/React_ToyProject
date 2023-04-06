9c060b0f8385158da966047c7be16d5d4c7465d8
Seoyun Son <sallyluckyyun@gmail.com>
2023-04-06 23:55:18 +0900

    ADD: css - from https://github.com/nomadcoders/react-for-beginners/commit/58aca2193a50ff21663ed4fb06b0785a8480c05e?diff=split

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -14,15 +15,19 @@ function Home() {
     getMovies()
   }, [])
   return (
-    <div className="App">
-      <h1>The movies! {loading ? "" : `(${movies.length})`}</h1>
-      {loading ? <strong>Loading...</strong> :
-        <div>
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) :
+        <div className={styles.movies}>
           {movies.map((movie) => <Movie
             key={movie.id}
             id={movie.id}
+            year={movie.year}
             coverImg={movie.medium_cover_image}
-            title={movie.title_long}
+            title={movie.title}
             rating={movie.rating}
             summary={movie.summary}
             genres={movie.genres}
